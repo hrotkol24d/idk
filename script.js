1b692b1800467b46c2a4c063704d4c5c70e0a963
hrotkol24d <hrotkol.24d@acsjszki.hu>
2025-03-06 10:54:48 +0100

    levimozija

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-let fname = "John"; let lname = "Doe"; let age = 25; age = 25;
+var quantity = prompt("Add meg a darabszámot");
+var type = prompt("Add meg a típust");
+
+var types = {
+  student: 300,
+  adult: 350,
+  retired: 280
+};
+
+var price = types[type];
+
+if(price === undefined) {
+    alert('érvénytelen típus');
+} else {
+    var discount = quantity > 10 ? 0.9 : 1;
+    var total = quantity * price * discount;
+    alert(total);
+}
